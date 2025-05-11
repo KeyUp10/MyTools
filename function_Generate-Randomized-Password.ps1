194384813a4fa6bda97e6bea7569edfaae0e795c
KeyUp10 <register25190-onetime@yahoo.co.jp>
2025-05-12 00:56:22 +0900

    KeyUp10

---
+++ function_Generate-Randomized-Password.ps1
@@ -0,0 +1,9 @@
+﻿#ランダムな文字列を生成する関数
+
+Add-type -AssemblyName System.Web
+
+function Generate-Randomized-Password([int]$a, [int]$b) {
+
+     [System.Web.Security.Membership]::GeneratePassword($a, $b)
+
+}
