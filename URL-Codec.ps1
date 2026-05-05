03e0dc0158316588da0bb3c8820a9c569dcd4019
KeyUp10 <register25190-onetime@yahoo.co.jp>
2026-05-05 14:51:39 +0900

    KeyUp10

---
+++ URL-Codec.ps1
@@ -0,0 +1,13 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("encode","decode")]
+    [string]$Mode,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Text
+)
+
+switch ($Mode) {
+    "encode" { [System.Net.WebUtility]::UrlEncode($Text) }
+    "decode" { [System.Net.WebUtility]::UrlDecode($Text) }
+}
